d29965e43841dbb519feb7804f261fb68e2073f2
Rex Whitten <rwhitten@acrowire.com>
2015-01-29 09:23:57 -0500

    Adding the basic version of MS SSQL Data access

---
+++ Acrowire.Data/DataAccessBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acrowire.Data
+{
+    public partial class DataAccessBase
+    {
+
+        #region [ Fields ] 
+        private SqlDataAccess m_access;
+        #endregion
+
+        #region [ Properies ]
+        public String ConnectionStringName { get; private set; }
+
+        public SqlDataAccess Access { get; set; }
+        #endregion
+
+        #region [ Constructors ]
+        public DataAccessBase()
+        {
+            this.ConnectionStringName = "default";
+            this.Access = new SqlDataAccess(this.ConnectionStringName);
+        }
+
+        #endregion
+
+    }
+}
